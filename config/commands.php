31e92c753947974cbcaf6c60dcdbc17abeab4bc6
Keoghan Litchfield <keoghan@klever.co.uk>
2018-07-30 13:06:50 +0100

    Hide database commands

--- config/commands.php
+++ config/commands.php
@@ -63,6 +63,17 @@ return [
         \LaravelZero\Framework\Commands\App\CommandMaker::class,
 
         \Intonate\TinkerZero\Console\TinkerZeroCommand::class,
+
+        \Illuminate\Database\Console\Migrations\InstallCommand::class,
+        \Illuminate\Database\Console\Migrations\FreshCommand::class,
+        \Illuminate\Database\Console\Migrations\MigrateCommand::class,
+        \Illuminate\Database\Console\Migrations\MigrateMakeCommand::class,
+        \Illuminate\Database\Console\Migrations\RefreshCommand::class,
+        \Illuminate\Database\Console\Migrations\ResetCommand::class,
+        \Illuminate\Database\Console\Migrations\RollbackCommand::class,
+        \Illuminate\Database\Console\Migrations\StatusCommand::class,
+        \Illuminate\Database\Console\Seeds\SeedCommand::class,
+        \Illuminate\Database\Console\Seeds\SeederMakeCommand::class,
     ],
 
     /*
