5740754f33810c09f8a867a6ff48e90be933222f
Robin D'Arcy <robin@klever.co.uk>
2018-08-01 22:02:43 +0100

    Fix database creation tests

--- tests/Unit/Support/Database/DatabaseTest.php
+++ tests/Unit/Support/Database/DatabaseTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Support\Database;
 
 use App\Support\Database\Database;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
@@ -14,40 +15,39 @@ class DatabaseTest extends TestCase
     {
         parent::setUp();
 
-        $this->cleanseDir(storage_path());
-
-        $this->databasePath = storage_path('temp/test_database.sqlite');
-        $this->app->config['database.connections.default.database'] = $this->databasePath;
+        $this->cleanseDir(storage_path('temp'));
     }
 
-    protected function tearDown()
+    public function tearDown()
     {
-        $this->cleanseDir(storage_path());
+        $this->cleanseDir(storage_path('temp'));
     }
 
     /** @test */
     public function it_creates_a_database_file_if_one_does_not_exist()
     {
+        $this->databasePath = storage_path('temp/testing.sqlite');
+        $this->app->config['database.connections.default.database'] = $this->databasePath;
+
         $this->assertFileNotExists($this->databasePath);
         $this->assertFalse(Database::exists());
-        Database::ensureExists();
-        $this->assertFileExists($this->databasePath);
-        $this->assertTrue(Database::exists());
 
-        // Check that migrations have been run
-        $this->assertGreaterThan(0, DB::table('migrations')->count());
+        Artisan::shouldReceive('call')->withArgs(['migrate:fresh']);
+        Artisan::shouldReceive('call')->withArgs(['db:seed']);
+
+        Database::ensureExists();
+
+        $this->assertFileExists($this->databasePath);
     }
 
     /** @test */
     public function it_can_force_refresh_the_database_if_it_already_exists()
     {
-        if (! is_dir(dirname($this->databasePath))) {
-            mkdir(dirname($this->databasePath), 0777, true);
-        }
+        $this->assertTrue(Database::exists());
 
-        touch($this->databasePath);
+        Artisan::shouldReceive('call')->withArgs(['migrate:fresh']);
+        Artisan::shouldReceive('call')->withArgs(['db:seed']);
+
         Database::ensureExists(true);
-
-        $this->assertNotEmpty(file_get_contents($this->databasePath));
     }
 }
