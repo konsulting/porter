6b6bf5acc2ed7ca2ff830fd894e201e528338e85
Keoghan Litchfield <keoghan@Keoghans-MacBook-Pro.local>
2018-08-19 21:10:38 +0100

    Fix mysql environment vars

--- resources/views/docker_compose/konsulting/porter-ubuntu/mysql.blade.php
+++ resources/views/docker_compose/konsulting/porter-ubuntu/mysql.blade.php
@@ -3,13 +3,12 @@
     ports:
       - 13306:3306
     environment:
-      MYSQL_ROOT_PASSWORD: secret
+      - MYSQL_ROOT_PASSWORD=secret
+      - MYSQL_HISTFILE=/root/.mysql_history/history
     volumes:
       - ./storage/data/mysql:/var/lib/mysql
       - ./storage/logs/mysql:/var/log/mysql
       - ./storage/config/mysql/history:/root/.mysql_history
-    environment:
-      - MYSQL_HISTFILE=/root/.mysql_history/history
     networks:
       - porter
     restart: unless-stopped
