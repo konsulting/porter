bd23cd77cec134d081f7ae8a55388370067dbcff
Robin D'Arcy <robin@klever.co.uk>
2018-08-23 14:01:42 +0100

    Add container bindings for image repo and CLI runner contracts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Providers;
 
 use App\Porter;
 use App\Ssl\CertificateBuilder;
+use App\Support\Contracts\Cli as CliContract;
 use App\Support\Cli;
 use App\Support\ConsoleWriter;
+use App\Support\Images\ImageRepository;
+use App\Support\Contracts\ImageRepository as ImageRepositoryContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,7 +26,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(Cli::class);
+        $this->app->bind(CliContract::class, function () {
+            return new Cli;
+        });
+        $this->app->bind(ImageRepositoryContract::class, function () {
+            return new ImageRepository;
+        });
+
         $this->app->singleton(Porter::class);
         $this->app->singleton(ConsoleWriter::class);
         $this->app->singleton(CertificateBuilder::class, function () {
