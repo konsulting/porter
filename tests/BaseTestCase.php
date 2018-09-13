d14f88eb5bda6f3618b27ce36f159c93a62a48ad
Keoghan Litchfield <keoghan@klever.co.uk>
2018-09-13 17:56:16 +0100

    Fix testing set up, where Illuminate Traits were trying to work with non-existant files.

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -31,25 +31,6 @@ abstract class BaseTestCase extends IlluminateTestCase
             return;
         }
 
-        $this->app = $this->createApplication();
-
-        $this->cleanseDir(storage_path('test_library'));
-        @rmdir(storage_path('test_library'));
-
-        mkdir(storage_path('test_library'));
-        mkdir(storage_path('test_library/ssl'));
-        mkdir(storage_path('test_library/config/nginx/conf.d/'), 0777, true);
-
-        touch(storage_path('test_library') . '/testing.sqlite');
-
-        $this->app['config']->set('database.connections.default.database', storage_path('test_library') . '/testing.sqlite');
-        $this->app['config']->set('porter.docker-compose-file', storage_path('test_library/docker-compose.yaml'));
-        $this->app['config']->set('porter.library_path', storage_path('test_library'));
-
-        AppServiceProvider::$publishes[AppServiceProvider::class] = [
-            resource_path('stubs/config') => config('porter.library_path') . '/config'
-        ];
-
         $this->performMigrations();
 
         static::$migrated = true;
@@ -58,42 +39,5 @@ abstract class BaseTestCase extends IlluminateTestCase
     protected function performMigrations()
     {
 
-    }
-
-    protected function cleanseDir($dir)
-    {
-        if (! file_exists($dir)) {
-            return;
-        }
-
-        foreach (scandir($dir) as $item) {
-            if ($item == '.' || $item == '..') {
-                continue;
-            }
-
-            $current = $dir . DIRECTORY_SEPARATOR . $item;
-
-            if (is_dir($current)) {
-                $this->removeDir($current);
-                continue;
-            }
-
-            unlink($current);
-        }
-    }
-
-    protected function removeDir($dir)
-    {
-        if (! file_exists($dir)) {
-            return true;
-        }
-
-        if (! is_dir($dir)) {
-            throw new \Exception($dir . ' is not a directory');
-        }
-
-        $this->cleanseDir($dir);
-
-        return rmdir($dir);
     }
 }
--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Providers\AppServiceProvider;
 use LaravelZero\Framework\Kernel;
 
 trait CreatesApplication
@@ -17,6 +18,80 @@ trait CreatesApplication
 
         $app->make(Kernel::class)->bootstrap();
 
+        $this->setupStorage();
+
+        $app['config']->set('database.connections.default.database', storage_path('test_library') . '/testing.sqlite');
+        $app['config']->set('porter.docker-compose-file', storage_path('test_library/docker-compose.yaml'));
+        $app['config']->set('porter.library_path', storage_path('test_library'));
+
+        AppServiceProvider::$publishes[AppServiceProvider::class] = [
+            resource_path('stubs/config') => config('porter.library_path') . '/config'
+        ];
+
         return $app;
+    }
+
+    /**
+     * Setup local storage for running the tests
+     */
+    public function setupStorage()
+    {
+        $this->removeDir(storage_path('test_library'));
+
+        mkdir(storage_path('test_library'));
+        mkdir(storage_path('test_library/ssl'));
+        mkdir(storage_path('test_library/config/nginx/conf.d/'), 0777, true);
+
+        touch(storage_path('test_library') . '/testing.sqlite');
+    }
+
+    /**
+     * Clean out the contents of a directory recursively
+     *
+     * @param $dir
+     * @throws \Exception
+     */
+    protected function cleanseDir($dir)
+    {
+        if (! file_exists($dir)) {
+            return;
+        }
+
+        foreach (scandir($dir) as $item) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+
+            $current = $dir . DIRECTORY_SEPARATOR . $item;
+
+            if (is_dir($current)) {
+                $this->removeDir($current);
+                continue;
+            }
+
+            unlink($current);
+        }
+    }
+
+    /**
+     * Remove a directory
+     *
+     * @param $dir
+     * @return bool
+     * @throws \Exception
+     */
+    protected function removeDir($dir)
+    {
+        if (! file_exists($dir)) {
+            return true;
+        }
+
+        if (! is_dir($dir)) {
+            throw new \Exception($dir . ' is not a directory');
+        }
+
+        $this->cleanseDir($dir);
+
+        return rmdir($dir);
     }
 }
