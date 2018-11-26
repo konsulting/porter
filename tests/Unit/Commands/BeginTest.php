a10d07385ccac076baf84cb6b16536c3bd21016c
Robin D'Arcy <robin@klever.co.uk>
2018-11-26 12:00:42 +0000

    Prompt for home dir in begin command, add argument descriptions

--- tests/Unit/Commands/BeginTest.php
+++ tests/Unit/Commands/BeginTest.php
@@ -3,15 +3,17 @@
 namespace Tests\Unit\Commands;
 
 use App\Support\Images\Organiser\Organiser;
+use Illuminate\Support\Facades\Artisan;
 use Tests\BaseTestCase;
 
 class BeginTest extends BaseTestCase
 {
     protected $organiser;
 
-    public function setUp() : void
+    public function setUp(): void
     {
         $this->organiser = \Mockery::mock(Organiser::class);
+        $this->organiser->expects('pullImages');
 
         $this->afterApplicationCreated(function () {
             app()->bind(Organiser::class, function () {
@@ -23,10 +25,32 @@ class BeginTest extends BaseTestCase
     }
 
     /** @test */
-    public function it_pulls_the_docker_images()
+    public function it_prompts_for_the_home_directory()
     {
-        $this->organiser->expects('pullImages');
-
         $this->artisan('begin', ['--force' => true]);
+
+        $expected = 'Please enter the root directory for your sites, or leave blank to use the current directory.';
+        $this->stringContains($expected)->evaluate(Artisan::output());
+    }
+
+    /** @test */
+    public function it_uses_the_supplied_home_directory()
+    {
+        $home = storage_path('temp/test_home');
+        file_exists($home) ? null : mkdir($home, 0777, true);
+
+        $this->artisan('begin', ['--force' => true, 'home' => $home]);
+
+        $expected = "Setting home to {$home}";
+        $this->stringContains($expected)->evaluate(Artisan::output());
+    }
+
+    /** @test */
+    public function it_uses_the_current_directory_if_no_interaction()
+    {
+        $this->artisan('begin', ['--force' => true, '--no-interaction' => true]);
+
+        $expected = 'Setting home to ' . __DIR__;
+        $this->stringContains($expected)->evaluate(Artisan::output());
     }
 }
