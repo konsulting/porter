5d674c8986841297f1fb901d46280a3b69ce5d5d
Keoghan Litchfield <keoghan@klever.co.uk>
2019-01-16 11:45:29 +0000

    Adjust composer setup so we can store other setup items, such as github tokens for use across containers

--- resources/image_sets/konsulting/porter-ubuntu/docker_compose/php_cli.blade.php
+++ resources/image_sets/konsulting/porter-ubuntu/docker_compose/php_cli.blade.php
@@ -7,7 +7,7 @@
       - porter
     volumes:
       - {{ $home }}:/srv/app:delegated
-      - {{ $libraryPath }}/composer/cache:/root/.composer/cache
+      - {{ $libraryPath }}/composer:/root/.composer
       - {{ $libraryPath }}/config/user/ssh:/root/.ssh
       - {{ $libraryPath }}/config/{{ $version->cli_name }}/php.ini:/etc/php/{{ $version->version_number }}/cli/php.ini
       - {{ $libraryPath }}/config/{{ $version->cli_name }}/xdebug.ini:/etc/php/{{ $version->version_number }}/cli/conf.d/xdebug.ini
