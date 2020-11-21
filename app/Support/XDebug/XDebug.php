ef34960c974bc3a34bfa0a9d32d7c7ed5e0506c8
Keoghan Litchfield <keoghan@klever.co.uk>
2020-11-21 13:41:54 +0000

    Clean up Xdebug class

--- app/Support/XDebug/XDebug.php
+++ app/Support/XDebug/XDebug.php
@@ -25,19 +25,9 @@ class XDebug
     {
         Setting::updateOrCreate('use_xdebug', 'on');
 
-        $results = [];
         foreach (PhpVersion::active()->get() as $version) {
-            $move = "mv /etc/php/{$version->version_number}/mods-available/xdebug.bak /etc/php/{$version->version_number}/mods-available/xdebug.ini";
-
-            ob_start();
-            $this->dockerCompose->execContainer($version->getFpmNameAttribute())->append($move)->interactive()->perform();
-            $results[$version->id] = ob_get_clean();
-        }
-
-        foreach (PhpVersion::active()->get() as $version) {
-            if ($results[$version->id]) {
-                // If there was any output captured it's because it failed - the file wasn't able to be moved
-                // Most likely because it was moved before
+            if (! $this->enable($version)) {
+                // If enable fails it's because it was already enabled
                 continue;
             }
 
@@ -49,23 +39,62 @@ class XDebug
     {
         Setting::updateOrCreate('use_xdebug', 'off');
 
-        $results = [];
         foreach (PhpVersion::active()->get() as $version) {
-            $move = "mv /etc/php/{$version->version_number}/mods-available/xdebug.ini /etc/php/{$version->version_number}/mods-available/xdebug.bak";
-
-            ob_start();
-            $this->dockerCompose->execContainer($version->getFpmNameAttribute())->append($move)->interactive()->perform();
-            $results[$version->id] = ob_get_clean();
-        }
-
-        foreach (PhpVersion::active()->get() as $version) {
-            if ($results[$version->id]) {
-                // If there was any output captured it's because it failed - the file wasn't able to be moved
-                // Most likely because it was moved before
+            if (! $this->disable($version)) {
+                // If disable fails it's because it was already disabled
                 continue;
             }
 
             $this->porter->softRestart($version->getFpmNameAttribute());
         }
+    }
+
+    /**
+     * Move the ini file to .ini to enable
+     *
+     * @param  PhpVersion  $version
+     *
+     * @return bool
+     */
+    protected function enable(PhpVersion $version)
+    {
+        return $this->moveIniFile($version, 'xdebug.bak', 'xdebug.ini');
+    }
+
+    /**
+     * Move the ini file to .bak to disable
+     *
+     * @param  PhpVersion  $version
+     *
+     * @return bool
+     */
+    protected function disable(PhpVersion $version)
+    {
+        return $this->moveIniFile($version, 'xdebug.ini', 'xdebug.bak');
+    }
+
+    /**
+     * Move the ini file
+     *
+     * If there was any output captured it's because it failed - the file wasn't able to be moved
+     * Most likely because it was moved before
+     *
+     * @param  PhpVersion  $version
+     * @param $from
+     * @param $to
+     *
+     * @return bool
+     */
+    protected function moveIniFile(PhpVersion $version, $from, $to)
+    {
+        $move = "mv /etc/php/{$version->version_number}/mods-available/{$from} /etc/php/{$version->version_number}/mods-available/{$to}";
+
+        ob_start();
+        $this->dockerCompose->execContainer($version->getFpmNameAttribute())->append($move)->interactive()->perform();
+        if (ob_get_clean()) {
+            return false;
+        }
+
+        return true;
     }
 }
