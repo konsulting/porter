31b9bab4712507a10d1d30278bccf260f74dd7c8
Keoghan Litchfield <keoghan@klever.co.uk>
2023-03-30 16:34:38 +0100

    Minor adjustments to Meilsearch 1.0

--- resources/image_sets/konsulting/porter-ubuntu/docker_compose/meilisearch.blade.php
+++ resources/image_sets/konsulting/porter-ubuntu/docker_compose/meilisearch.blade.php
@@ -4,6 +4,8 @@
       - 17700:7700
     volumes:
       - {{ $libraryPath }}/data/meilisearch:/data.ms
+    environment:
+      - MEILI_MASTER_KEY=masterKey
     networks:
       - porter
     restart: unless-stopped
